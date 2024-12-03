2e4e82f23e69ab5f36166190c3e91373e4a74a24
Stephanie <sventura102@gmail.com>
2024-12-03 15:56:44 -0800

    created dashboard files and edited main page to include login

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}', // Update based on your project structure
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // If using app directory in Next.js 13 or newer
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
