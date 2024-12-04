f5eb42ef2eb272a507c9577a39a8f121762b1990
josem3129 <mar22069@byui.edu>
2024-12-03 18:47:42 -0700

    project change

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,51 @@
-import styles from "@/app/ui/style.module.css";
-import CardWarper from "./ui/card";
-import { Suspense } from "react";
-import React from "react";
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className={styles.main}>
-      <div className="w-[1440px] h-[1024px] relative bg-white">
-        <div className="w-[1196px] h-[248px] left-[244px] top-[123px] absolute bg-[#a5a5a5]" />
-        <div className="w-[394px] h-[77px] left-[645px] top-[33px] absolute text-black text-[40px] font-normal font-['Inter'] leading-[48.45px]">
-          Handcrafted Haven
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
+      </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          {/* <div className={styles.shape} /> */}
+          {/* <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" /> */}
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
+            <a href="https://nextjs.org/learn/" className="text-blue-500">
+              Next.js Learn Course
+            </a>
+            , brought to you by Vercel.
+          </p>
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
         </div>
-        <div className="w-[244px] h-[1024px] left-0 top-0 absolute bg-[#c8c8c8]" />
-        <div className="w-[88px] h-[88px] left-[1299px] top-[13px] absolute bg-[#a5a5a5]" />
-        <div className="w-[42px] h-[42px] left-[15px] top-[969px] absolute bg-[#e69f4e] rounded-full" />
-        <div className="w-[42px] h-[42px] left-[1322px] top-[29px] absolute bg-[#59cf4e] rounded-full" />
-        <div className="w-[42px] h-[42px] left-[1304px] top-[59px] absolute bg-[#59cf4e] rounded-full" />
-        <div className="w-[148px] h-[148px] left-[333px] top-[466px] absolute bg-[#bcbcbc] rounded-full" />
-        <div className="w-[148px] h-[148px] left-[554px] top-[466px] absolute bg-[#bcbcbc] rounded-full" />
-        <div className="w-[147px] h-[148px] left-[775px] top-[466px] absolute bg-[#bcbcbc] rounded-full" />
-        <div className="w-[148px] h-[148px] left-[995px] top-[466px] absolute bg-[#bcbcbc] rounded-full" />
-        <div className="w-[148px] h-[148px] left-[1216px] top-[466px] absolute bg-[#bcbcbc] rounded-full" />
-        <div className="w-[65px] h-[65px] left-[23px] top-[13px] absolute bg-[#bcbcbc] rounded-full" />
-        <div className="left-[285px] top-[740px] absolute justify-start items-center gap-[88px] inline-flex">
-          <div className="w-[216px] h-[206px] bg-[#a5a5a5]" />
-          <div className="w-[215px] h-[206px] bg-[#a5a5a5]" />
-          <div className="w-[216px] h-[206px] bg-[#a5a5a5]" />
-          <div className="w-[216px] h-[206px] bg-[#a5a5a5]" />
-        </div>
-        <div className="w-[212px] h-[46px] left-[15px] top-[80px] absolute bg-[#a5a5a5] rounded-[39px]" />
-        <div className="w-[198px] h-[30px] left-[285px] top-[691px] absolute text-black text-[32px] font-normal font-['Inter']">
-          For You
-        </div>
-        <div className="w-[198px] h-[30px] left-[23px] top-[186px] absolute text-black text-[32px] font-normal font-['Inter']">
-          For You
-        </div>
-        <div className="w-[198px] h-[30px] left-[22px] top-[247px] absolute text-black text-[32px] font-normal font-['Inter']">
-          <h2>shop</h2>
-          <Suspense>
-            <CardWarper />
-          </Suspense>
-        </div>
-        <div className="w-[198px] h-[30px] left-[22px] top-[308px] absolute text-black text-[32px] font-normal font-['Inter']">
-          Profile
-        </div>
-        <div className="w-[198px] h-[30px] left-[270px] top-[416px] absolute text-black text-[32px] font-normal font-['Inter']">
-          Categories
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
