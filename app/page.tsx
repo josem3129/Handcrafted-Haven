4344f8c05430eb82588af5ef1df40acc3b8d7856
Stephanie <sventura102@gmail.com>
2024-12-03 17:22:35 -0800

    main page ui added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
 import styles from "@/app/ui/style.module.css";
 import React from "react";
 import './global.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+      <main className="flex min-h-screen flex-col p-6">
+        <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
 
-    </main>
-  );
+        </div>
+        <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
+            <p className={` text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+              
+              <strong>Welcome to Handcrafted Haven.</strong> 
+              Click below to login and start searching!
+            </p>
+            <Link
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span className="w-5 md:w-10">Login</span>
+            </Link>
+          </div>
+        </div>
+      </main>
+    );
 }
