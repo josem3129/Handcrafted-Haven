d8834a3487d664e49b103d34991d01f4046f9623
josem3129 <mar22069@byui.edu>
2024-12-09 20:42:23 -0700

    added image adding

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -77,7 +77,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         {/* Listing description */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+            Description
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
@@ -87,6 +87,31 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 placeholder="Item description..."
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-auto text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="amount-error"
+              />
+            </div>
+            <div id="customer-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.amount &&
+                state.errors.amount.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
+          </div>
+        </div>
+          {/*image uploading */}
+          <div className="mb-4">
+          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
+            Add image
+          </label>
+          <div className="relative mt-2 rounded-md">
+            <div className="relative">
+              <input
+              type="file"
+              accept="image/*"
+                id="product_description"
+                placeholder="choose file"
+                className="peer block w-full rounded-md border border-gray-200 py-2 pl-auto text-sm outline-2 placeholder:text-gray-500"
               />
             </div>
             <div id="customer-error" aria-live="polite" aria-atomic="true">
@@ -113,6 +138,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           <p className="mt-2 text-sm text-red-500">{state.message}</p>
         ) : null}
       </div>
+
+      
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
