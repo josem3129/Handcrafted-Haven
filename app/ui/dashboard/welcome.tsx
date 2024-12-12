d7f50bfc3d5f615caeb0b2fec4d9071f7e2bf1f5
josem3129 <mar22069@byui.edu>
2024-12-11 20:13:41 -0700

    update 4

--- app/ui/dashboard/welcome.tsx
+++ app/ui/dashboard/welcome.tsx
@@ -18,7 +18,7 @@ export default async function GetUserWelcome (){
    
     return(
         <>
-        <WelcomeDashboard name={user.name}/>
+        <WelcomeDashboard/>
         </>
     )
     
--- app/ui/dashboard/welcomeSign.tsx
+++ app/ui/dashboard/welcomeSign.tsx
@@ -1,15 +1,10 @@
 import { playfair } from "../fonts";
 
-export function WelcomeDashboard({
-    name,
-  }: {
-    name: string;
-  }) {
+export function WelcomeDashboard() {
 
     return (
       <div>
         <h1 className={`${playfair.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
-        <h2 className={`${playfair.className} mb-4 text-xl md:text-2xl`}>Welcome {name}</h2>
       </div>
     );
   }
